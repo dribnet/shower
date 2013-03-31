838877fc0811c84db4c9969643964e585020592a
Tom White <tom@sixdozen.com>
2013-03-31 00:27:54 -0700

    color-magic: first version of working meteor app
    slow and code is a little clunky, but feature complete
    with js version.

--- project.clj
+++ project.clj
@@ -1,6 +1,7 @@
 (defproject shower "0.0.1-SNAPSHOT"
   :description "shower: experimental meteorjs library"
   :dependencies [[org.clojure/clojure "1.4.0"]
+                 [hiccups "0.2.0"]
                  [net.drib/strokes "0.4.1"]]
   :min-lein-version "2.0.0"
   :source-paths ["src/clj" "src/cljs"]
@@ -14,7 +15,7 @@
                         :jar true}
 
                       ; examples
-                       {:source-paths ["examples/color-magic"]
+                       {:source-paths ["src/cljs" "examples/color-magic"]
                         :compiler  {:optimizations :simple
                                     :pretty-print true
                                     :output-to "color-magic/color-magic.js"}}
--- src/cljs/shower.cljs
+++ src/cljs/shower.cljs
@@ -2,6 +2,9 @@
   (:require [mrhyde.core :as mrhyde]))
 
 (def Meteor (this-as ct (aget ct "Meteor")))
+(def Template (this-as ct (aget ct "Template")))
+(def Session (this-as ct (aget ct "Session")))
+(def Collection (aget Meteor "Collection"))
 
 (defn ^:export bootstrap []
   (mrhyde/bootstrap))
